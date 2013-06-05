f27e1cc1f0660e71218f3265ba03f893cd4654dc
Sam Macpherson <sam.mack91@gmail.com>
2013-06-05 15:11:21 +1000

    naive cross-thread signal/slot implementation, commit here since new threading model using moveToThread will have to be implemented.

--- src/AVRThread.cpp
+++ src/AVRThread.cpp
@@ -5,7 +5,31 @@
 #include "AVRThread.h"
 
 #include <sys/time.h>
+
 #include <stdint.h>
+
+#include "sim_avr.h"
+#include "avr_ioport.h"
+#include "sim_elf.h"
+#include "sim_gdb.h"
+
+typedef struct {
+  port_t port;
+  pin_t  pin;
+  char   type;
+} PinState_t;
+
+typedef char port_t;
+typedef char pin_t;
+
+static AVRThread* class_instance;
+
+void non_mem_irq_handler(struct avr_irq_t* irq, uint32_t value, void* param){
+
+  class_instance->irq_handler(irq,value,param);
+
+}
+
 
 AVRThread::AVRThread(){
 
@@ -24,9 +48,12 @@ AVRThread::AVRThread(){
 
   avr_load_firmware(this->avr, &firmware_bin);
 
-  this->verbose = false;
+  this->verbose = true;
 
   this->status = cpu_Running;
+
+  this->connect_irqs();
+
 }
 
 AVRThread::AVRThread(avr_t* avr){
@@ -58,6 +85,8 @@ AVRThread::AVRThread(QString filename){
   this->verbose = false;
 
   this->status = cpu_Running;
+
+  //this->frequency = 8000000; // 8 MHz
 }
 
 AVRThread::AVRThread(QString filename, QString mmcu, unsigned int frequency){
@@ -82,23 +111,85 @@ AVRThread::AVRThread(QString filename, QString mmcu, unsigned int frequency){
   this->verbose = false;
 
   this->status = cpu_Running;
+
+  //this->frequency = 8000000; // 8 MHz
 }
+
+AVRThread::AVRThread(const AVRThread& rhs){}
 
 void AVRThread::loop_delay(long count){
 
   if( count < 0 ) return;
 
+  //TODO "nop" may not be portable
   while(count--) asm("nop");
 
 }
 
-avr_t* AVRThread::getAVR(void){
-  return this->avr;
+
+void AVRThread::irq_handler(struct avr_irq_t* irq, uint32_t value, void* param){
+
+  // Suppress unused variable warnings
+  (void) irq;
+  (void) value;
+
+  PinState_t* pinState = (PinState_t*) &param;
+
+  //qDebug() << pinState->port << (int)(pinState->pin);
+
+  //emit this->pinChange(pinState->port, pinState->pin);
+
+  // if PortNPin->type == DDR_PORT_TYPE)
+    //emit this->ddChange(PortNPin->port, PortNPin->pin);
 }
+
+void AVRThread::connect_irqs(void){
+
+  PinState_t pinState;
+  void* param;
+  struct avr_irq_t* irq;
+
+  class_instance = this;
+
+  for(uint8_t i = 0; i < NUM_AVR_PORTS; i++){
+
+    for(uint8_t pin = 0; pin < 8; pin++){
+
+      // PortF:Pin0 = ('F' << 8 | 0)
+      //PortNPin = (PortPin_t) ( (ports[i] << 8) | j );
+
+      // Pray this is packed tight
+      pinState.port = AVR_PORT[i];
+      pinState.pin = pin;
+
+      memcpy((void*) &param, (void*) &pinState, sizeof(void*));
+
+      irq = avr_io_getirq(this->avr, AVR_IOCTL_IOPORT_GETIRQ(AVR_PORT[i]), pin);
+
+      avr_irq_register_notify(irq, non_mem_irq_handler, param );
+    }
+  }
+}
+
+
+// Slots
+////////////////////////////////////////////////////////////////////////////////
 
 void AVRThread::RESET(void){}
 
+void setFrequency(int frequency){}
+
+void andPort(port_t port, pin_t pin){}
+void orPort(port_t port, pin_t pin){}
+void xorPort(port_t port, pin_t pin){}
+
+void mutePort(port_t port, pin_t pin){}
+void unMutePort(port_t port, pin_t pin){}
+
 void AVRThread::run(void){
+  //TODO variable loop speed recalculation instead 1 sec
+  //TODO variable loop speep
+  //TODO usleep() instead of loop_delay()
 
   struct timeval t1, t2;
 
@@ -122,19 +213,20 @@ void AVRThread::run(void){
 
     cycles += (unsigned int) (this->avr->cycle - last_cycle_count);
 
-    dt = (t2.tv_sec - t1.tv_sec)*1000000 + (t2.tv_usec - t1.tv_usec);
+    dt = (t2.tv_sec - t1.tv_sec)*10000 + (t2.tv_usec - t1.tv_usec);
 
     /* One Second has passed, adjust loop speed */
-    if( dt >= 1000000 ){
+    if( dt >= 10000 ){
 
-      cfactor = cycles - 8000000;
+      //cfactor = cycles - this->frequency;
+      cfactor = cycles - 80000;
 
       /* TODO consider log(cfactor) or sqrt(cfactor)*/
-      loop_count += cfactor / 65536; 
+      loop_count += cfactor / 655;
 
       if(loop_count < 0) loop_count = 0;
 
-      if(verbose){
+      if(this->verbose){
 
         qDebug() << "AVRThread: Loop Count:" << loop_count << "cFactor:"
           << cfactor << "Freq:" << cycles;
@@ -143,6 +235,8 @@ void AVRThread::run(void){
       cycles = 0;
 
       gettimeofday(&t1, NULL);
+
+      QCoreApplication::processEvents();
 
     }
 
--- src/AVRThread.h
+++ src/AVRThread.h
@@ -7,27 +7,60 @@
 #include "sim_avr.h"
 #include "sim_elf.h"
 
+typedef char port_t;
+typedef char pin_t;
+
+const char NUM_AVR_PORTS = 7;
+const char AVR_PORT[NUM_AVR_PORTS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+
 class AVRThread : public QThread {
+
+  //Q_OBJECT
 
   public:
     AVRThread(QString filename, QString mmcu, unsigned int frequency);
     AVRThread(QString filename);
     AVRThread(avr_t* avr);
-    AVRThread();
-
-    avr_t* getAVR(void);
-
-    void RESET(void);
+    AVRThread(const AVRThread& rhs);
+    AVRThread(); // Prevent this case once testing is finished with a throw?
 
     void run(void);
 
-    bool verbose;
-    
+  public slots:
+
+    void RESET(void);
+    void setFrequency(int);
+
+    void andPort(port_t, pin_t);
+    void orPort(port_t, pin_t);
+    void xorPort(port_t, pin_t);
+
+    void mutePort(port_t, pin_t);
+    void unMutePort(port_t, pin_t);
+
+    // Handles IRQ's and emits corrosponding Qt signal
+    void irq_handler(struct avr_irq_t* irq, uint32_t value, void* param);
+
+  signals:
+
+    // Signalled when any port not muted changes level
+    //void pinChange(port_t, pin_t);
+
+    // Signalled when any port not muted changes direction i.e. Input/Ouput
+    //void ddChange(port_t, pin_t);
+
   private:
     avr_t* avr;
     int status;
+    int frequency;
+    bool verbose;
 
-    static void loop_delay(long loop_count);
+    void loop_delay(long loop_count);
+
+    // maps irq to irq_handler();
+    void connect_irqs(void);
+
+
 
 };
 
