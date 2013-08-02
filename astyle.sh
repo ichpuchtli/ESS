0f34db502966e245d0139d41dac722777a2aefc9
Sam Macpherson <sam.mack91@gmail.com>
2013-08-02 22:07:07 +1000

    astyle script now takes any number of arguments and styles each individually

--- astyle.sh
+++ astyle.sh
@@ -4,4 +4,4 @@ astyle --indent=spaces=2 --brackets=linux \
       --indent-labels --pad-oper --pad-paren-in --unpad-paren --pad-header \
       --keep-one-line-statements --convert-tabs \
       --indent-preprocessor --max-code-length=80 \
-      `git status -uno -s | cut -d" " -f3 | egrep '\.h$|\.cpp$'`
+      "$@"
