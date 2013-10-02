b808b52ed85aa9b0c22491faf0a06412926a8de2
Sam Macpherson <sam.mack91@gmail.com>
2013-10-02 17:16:13 +1000

    verbose output and now .orig backups

--- astyle.sh
+++ astyle.sh
@@ -3,5 +3,5 @@
 astyle --indent=spaces=2 --brackets=linux \
       --indent-labels --pad-oper --pad-paren-in --unpad-paren --pad-header \
       --keep-one-line-statements --convert-tabs \
-      --indent-preprocessor --max-code-length=80 \
+      --indent-preprocessor --max-code-length=80 --suffix=none -v\
       "$@"
